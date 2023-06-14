8b74bc58d84aa8e6977a9532f9748206dcb1cb12
Caelo <mike22541678@gmail.com>
2023-06-14 17:05:14 -0500

    Reset button fuction

--- JS/index.js
+++ JS/index.js
@@ -3,6 +3,7 @@ window.addEventListener('DOMContentLoaded',function(){
     // Constants
     const eyeButton = document.querySelector('.eyeScore');
     const eye = document.querySelector('.fa-eye');
+    const restart = document.querySelector('.heavensDoor');
     const eyeSlash = document.querySelector('.fa-eye-slash');
     const timer = document.querySelector('.timer');
     const textSection = document.querySelector('section');
@@ -20,11 +21,11 @@ window.addEventListener('DOMContentLoaded',function(){
     let startCountdown = true;
     let wrongPhrases = 0;
     let correctPhrases = 0;
-    let words;
     let number = 0;
     let number2 = 0;
+    let words;
     let pharagraphs = ['KonoSuba Gods Blessing on This Wonderful World! is a Japanese light novel series written by Natsume Akatsuki. The series follows Kazuma Satou, a boy who is sent to a fantasy world with MMORPG elements following his death, where he forms a dysfunctional adventuring party with a goddess, an archwizard, and a crusader. Originally serialized as a web novel on Shosetsuka ni Naro between December 2012 and October 2013, KonoSuba was published as a printed light novel series by Kadokawa Shoten under the companys Kadokawa Sneaker Bunko imprint from October 2013 to May 2020. The light novel series features a divergent plot and illustrations by Kurone Mishima.',
-    'Miss Kobayashis Dragon Maid also known as The Maid Dragon of Kobayashi-san is a Japanese manga series written and illustrated by Coolkyousinnjya. The series began serialization in Futabashas Monthly Action magazine since May 2013 and is licensed in North America by Seven Seas Entertainment. Four spin-off manga have also been serialized in Monthly Action. An anime television series adaptation produced by Kyoto Animation aired in Japan between January and April 2017. A second season titled Miss Kobayashis Dragon Maid S aired between July and September 2021. A video game titled Miss Kobayashis Dragon Maid: Burst Forth!! Choro-gon Breath was released in Japan in March 2022 and was released in North America by Aksys Games in August 2022.',
+    'Miss Kobayashis Dragon Maid also known as The Maid Dragon of Kobayashi san is a Japanese manga series written and illustrated by Coolkyousinnjya. The series began serialization in Futabashas Monthly Action magazine since May 2013 and is licensed in North America by Seven Seas Entertainment. Four spin-off manga have also been serialized in Monthly Action. An anime television series adaptation produced by Kyoto Animation aired in Japan between January and April 2017. A second season titled Miss Kobayashis Dragon Maid S aired between July and September 2021. A video game titled Miss Kobayashis Dragon Maid: Burst Forth!! Choro-gon Breath was released in Japan in March 2022 and was released in North America by Aksys Games in August 2022.',
     'Naruto is a Japanese manga series written and illustrated by Masashi Kishimoto. It tells the story of Naruto Uzumaki, a young ninja who seeks recognition from his peers and dreams of becoming the Hokage, the leader of his village. The story is told in two parts the first set in Narutos pre-teen years, and the second in his teens. The series is based on two one-shot manga by Kishimoto: Karakuri (1995), which earned Kishimoto an honorable mention in Shueishas monthly Hop Step Award the following year, and Naruto (1997).',
     'Onimai: Im Now Your Sister! is a Japanese manga series written and illustrated by Nekotofu. The series has been serialized online since 2017; it is also currently serialized in Ichijinshas Monthly Comic Rex magazine since April 2019. It is licensed in English by Kodansha USA. An anime television series adaptation by Studio Bind aired from January to March 2023, receiving positive reception from critics and gaining high ratings, although it received a mixed reception from English language critics.',
     'Fullmetal Alchemist: Brotherhood is a Japanese anime television series adapted from the original Fullmetal Alchemist manga series by Hiromu Arakawa. Produced by Bones, the series is directed by Yasuhiro Irie, written by Hiroshi onogi and composed by Akira Senju. The series was conceived in order to create a faithful adaptation that directly follows the entire storyline of the original manga, after 2003s Fullmetal Alchemist anime series strayed away from it to tell its own story after running out of published manga material to adapt.',
@@ -44,30 +45,15 @@ window.addEventListener('DOMContentLoaded',function(){
       showTimer = !showTimer;
       localStorage.setItem('showTimer',showTimer)
     })
-    input.addEventListener('keydown',function(e){
+    input.addEventListener('keydown',function (e){
       if(startCountdown == true){
         timerCoultdown()
         startCountdown = false;
       }
-      const wordSelected = document.querySelectorAll('section span');
-      if(e.key === " "){
-        if(wordSelected[selectWord].textContent == e.target.value.trim()){
-          wordSelected[selectWord].classList.add('correctValue')
-          wordSelected[selectWord].classList.add('afterValue')
-          wordSelected[selectWord].classList.remove('wrongValue')
-          correctPhrases++;
-          correctWord.textContent = correctPhrases
-        } else {
-          wordSelected[selectWord].classList.add('wrongValue')
-          wordSelected[selectWord].classList.add('afterValue')
-          wordSelected[selectWord].classList.remove('correctValue')
-          wrongPhrases++;
-          wrongWord.textContent = wrongPhrases
-        }
-        e.target.value = '';
-        selectWord++;
-        wordSelected[selectWord].classList.add('active');
-      }
+        inputStart(e)
+    })
+    restart.addEventListener('click',function(){
+      reset()
     })
     // ---------
 
@@ -98,7 +84,7 @@ window.addEventListener('DOMContentLoaded',function(){
     // ---------
 
     // Fuctions
-    function sectionSpans(){  /* Fuctions for the text in the section */
+      function sectionSpans(){  /* Fuctions for the text in the section */
       words = pharagraphs[Math.floor(Math.random(6) * 6)]
 
       words = words.split(' ')
@@ -114,14 +100,50 @@ window.addEventListener('DOMContentLoaded',function(){
       textSection.children[0].classList.add('active')
     }
 
+    function inputStart(e){
+      const wordSelected = document.querySelectorAll('section span');
+      if(e.key === " "){
+        if(wordSelected[selectWord].textContent == e.target.value.trim()){
+          wordSelected[selectWord].classList.add('correctValue')
+          wordSelected[selectWord].classList.add('afterValue')
+          wordSelected[selectWord].classList.remove('wrongValue')
+          correctPhrases++;
+          correctWord.textContent = correctPhrases
+        } else {
+          wordSelected[selectWord].classList.add('wrongValue')
+          wordSelected[selectWord].classList.add('afterValue')
+          wordSelected[selectWord].classList.remove('correctValue')
+          wrongPhrases++;
+          wrongWord.textContent = wrongPhrases
+        }
+        e.target.value = '';
+        selectWord++;
+        wordSelected[selectWord].classList.add('active');
+      }
+    }
+    
+    function reset(){
+        wrongWord.textContent = '0';
+        correctWord.textContent = '0';
+        selectWord = 0;
+        startCountdown = true;
+        wrongPhrases = 0;
+        correctPhrases = 0;
+        number = 0;
+        number2 = 0;
+        textSection.textContent = '';
+        sectionSpans()
+    }
+
     function timerCoultdown() { /* Timer fuction for the time in the aside */
       number++;
       if (number === 60) {
         number = 0;
         number2++;
-        if (number2 === 60) {
-          timer.children[0].textContent = 'Timeout!';
-          return;
+        if (number2 === 60 && number === 30) {
+            timer.children[0].textContent = 'Timeout!';
+            alert('You do ' + correctPhrases + ' WPM')
+            return;
         }
       }
       timer.children[0].textContent = number2 + ':' + (number < 10 ? '0' : '') + number;
